946558eac02da9348794808472d13c0bb34581dc
OCG071 <omarcg071@gmail.com>
2023-04-19 08:30:11 -0600

    commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tour of heroes';
+}
+
